8baa19b23439febf26987778455e6097b8cf03ad
Jack Moran <jvckmorvn@gmail.com>
2023-10-18 18:18:52 +0200

    dark mode switch

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import App from './App';
 import theme from './theme';
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -13,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <App />
     </ChakraProvider>
   </React.StrictMode>
-)
+);
