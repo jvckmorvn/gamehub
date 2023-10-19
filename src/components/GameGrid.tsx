30e4302aba4fdac7df6f76d1520f77bee661dd97
Jack Moran <jvckmorvn@gmail.com>
2023-10-19 23:52:18 +0200

    extract fetch hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,7 @@ import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 
 function GameGrid() {
-  const {games, error, isLoading} = useGames();
+  const {data, error, isLoading} = useGames();
   const skeletons= [1, 2, 3, 4, 5, 6, 7, 8];
   
   return (
@@ -15,7 +15,7 @@ function GameGrid() {
         padding={10}
         spacing={10}>
         {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCard key={game.id} game={game}/>
         ))}
       </SimpleGrid>
