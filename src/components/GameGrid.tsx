cb60da8676860a98277237c4feff04cd1d286361
Jack Moran <jvckmorvn@gmail.com>
2023-10-20 20:07:48 +0200

    refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,27 +12,28 @@ interface Props {
 function GameGrid({gameQuery}: Props) {
   const {data, error, isLoading} = useGames(gameQuery);
   const skeletons= [1, 2, 3, 4, 5, 6, 7, 8];
-  const bgColour = useColorModeValue('#edf2f7', '#2d3748');
-  
+  const bgColour = useColorModeValue('#e2e8f0', '#2d3748');
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{sm: 1, md: 2, lg: 3, xl: 4}}
-        padding={10}
-        spacing={6}>
-        {isLoading && skeletons.map(skeleton => (
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton bgColour={bgColour}/>
-          </GameCardContainer>
-        ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} bgColour={bgColour}/>
-          </GameCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{sm: 1, md: 2, lg: 3, xl: 4}}
+      padding={10}
+      spacing={6}>
+      {isLoading && skeletons.map(skeleton => (
+        <GameCardContainer key={skeleton}>
+          <GameCardSkeleton bgColour={bgColour}/>
+        </GameCardContainer>
+      ))}
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} bgColour={bgColour}/>
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 }
 
