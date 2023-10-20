ec23c9ab320156150f8721aee4f1af8f068e8bad
Jack Moran <jvckmorvn@gmail.com>
2023-10-20 12:44:17 +0200

    sort selector

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from './components/SortSelector';
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
       <GridItem gridArea="main">
         <HStack spacing={4} paddingLeft={10}>
           <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={platform => setGameQuery({...gameQuery, platform})}/>
-          <SortSelector/>
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
         </HStack>
         <GameGrid gameQuery={gameQuery}/>
       </GridItem>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,7 +16,15 @@ export interface Game {
 }
 
 function useGames(gameQuery: GameQuery) {
-  return useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id}}, [gameQuery]);
+  return useData<Game>(
+    '/games',
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder
+      }
+    }, [gameQuery]);
 }
 
 export default useGames;
