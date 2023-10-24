8ed4de5560a014aac626b4db41d1268b8a74f2b6
Jack Moran <jvckmorvn@gmail.com>
2023-10-24 20:07:14 +0200

    infinite queries

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,17 @@
-import { SimpleGrid, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, SimpleGrid, Text, useColorModeValue } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 import { GameQuery } from '../App';
+import React from 'react';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 function GameGrid({gameQuery}: Props) {
-  const {data, error, isLoading} = useGames(gameQuery);
+  const {data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage} = useGames(gameQuery);
   const skeletons= [1, 2, 3, 4, 5, 6, 7, 8];
   const bgColour = useColorModeValue('#e2e8f0', '#2d3748');
 
@@ -19,21 +20,31 @@ function GameGrid({gameQuery}: Props) {
   }
 
   return (
-    <SimpleGrid
-      columns={{sm: 1, md: 2, lg: 3, xl: 4}}
-      padding={10}
-      spacing={6}>
-      {isLoading && skeletons.map(skeleton => (
-        <GameCardContainer key={skeleton}>
-          <GameCardSkeleton bgColour={bgColour}/>
-        </GameCardContainer>
-      ))}
-      {data?.results.map((game) => (
-        <GameCardContainer key={game.id}>
-          <GameCard game={game} bgColour={bgColour}/>
-        </GameCardContainer>
-      ))}
-    </SimpleGrid>
+    <Box padding={10}>
+      <SimpleGrid
+        columns={{sm: 1, md: 2, lg: 3, xl: 4}}
+        spacing={6}>
+        {isLoading && skeletons.map(skeleton => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton bgColour={bgColour}/>
+          </GameCardContainer>
+        ))}
+        {data?.pages.map((page, index) => 
+          <React.Fragment key={index}>
+            {page.results.map(game => 
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} bgColour={bgColour}/>
+              </GameCardContainer>
+            )}
+          </React.Fragment>
+        )}
+      </SimpleGrid>
+      {hasNextPage && 
+        <Button onClick={() => fetchNextPage()} marginBottom={3} marginTop={9}>
+          {isFetchingNextPage ? 'Loading...' : 'Load More'}
+        </Button>
+      }
+    </Box>
   );
 }
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import ApiClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -16,16 +16,21 @@ export interface Game {
 }
 
 function useGames(gameQuery: GameQuery) {
-  return useQuery<FetchResponse<Game>, Error>({
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => apiClient.getAll({
-      params: {
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      }
-    })
+    queryFn: ({pageParam = 1}) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam
+        }
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    }
   });
 }
 
