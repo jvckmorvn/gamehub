854e894ea76e7caabe6c6535d638549a98bcfc47
Jack Moran <jvckmorvn@gmail.com>
2023-10-28 23:21:43 +0200

    refactor interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import ApiClient from '../services/api-client';
 import time from '../utils/time';
-import { Genre } from '../entities/Genre';
+import Genre from '../entities/Genre';
 
 const apiClient = new ApiClient<Genre>('/genres');
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import ApiClient from '../services/api-client';
 import time from '../utils/time';
-import { Platform } from '../entities/Platform';
+import Platform from '../entities/Platform';
 
 const apiClient = new ApiClient<Platform>('/platforms/lists/parents');
 
